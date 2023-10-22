6329684374d0d06e90a04f9cf93b0068767580bc
Eduardo-Soares-Sousa <eduardo_soares001@outlook.com>
2023-10-22 15:18:46 -0300

    Commit inicial

---
+++ PedraPapelTesoura/Coisa.java
@@ -0,0 +1,13 @@
+package PedraPapelTesoura;
+
+//classe abstrata:
+public abstract class Coisa {
+    //não há necessidade de atributos:
+    //------------------------------------
+
+    //não há necessidade de construtor:
+    //------------------------------------
+
+    //Declaração do método abstrato: 
+    protected abstract int result(Coisa opcao);  
+}
---
+++ PedraPapelTesoura/Jogador.java
@@ -0,0 +1,23 @@
+package PedraPapelTesoura;
+
+public class Jogador {
+    private String namePlayer;
+    private Coisa escolha;
+
+    public Jogador(String namePlayer){
+        this.namePlayer = namePlayer;
+    }
+
+    public void jogador(Coisa escolha){
+        this.escolha = escolha;
+    }
+
+    public String getNamePlayer(){
+        return namePlayer;
+    }
+
+    public Coisa getEscolha(){
+        return escolha;
+    }   
+    
+}
